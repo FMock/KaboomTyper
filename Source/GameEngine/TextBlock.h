aef09bc46ebdd09cf5c9d936a666962d7fc2ccde
FMock <FMock@users.noreply.github.com>
2024-02-18 21:12:53 -0800

    Improved TextBlock to TextString draw size

--- Source/GameEngine/TextBlock.h
+++ Source/GameEngine/TextBlock.h
@@ -1,6 +1,7 @@
 #pragma once
 #ifndef TEXTBLOCK_H
 #define TEXTBLOCK_H
+
 #include "sprite.h"
 #include <GL/glew.h>
 #include <map>
@@ -25,38 +26,33 @@ namespace GameEngine
 		TextBlock() = default;
 		TextBlock(int, int, int, int, std::string, std::map<std::string, GLuint>& tMap);
 		TextBlock(int, int, TextBlockParameters&, TextStringFont&, std::string, std::map<std::string, GLuint>& tMap);
-		//TextBlock(int, int, TextBlockParameters&, TextString&, std::string, std::map<std::string, GLuint>& tMap);
 		~TextBlock();
-		TextString* m_textString;
 
+		std::string m_text;        // the text for the TextString
+		int m_textSize;
+		TextString* m_textString; // TextString that's drawn over the TextBlock
 		std::map<std::string, GLuint> m_strToImageMap;
-
 		std::vector<std::string> m_colors;
-
 		std::string m_color;
 
-		std::string m_text; // the text to be displayed on the block
+		int m_fontWidth;
+		int m_fontHeight;
 
-		int fontWidth;
-		int fontHeight;
-
-		// The direction the character is facing
-		int direction;
-
-		int prev_change_x;
-
-		int prev_change_y;
+		// The direction the textblock is moving
+		int m_direction;
+		int m_prev_change_x;
+		int m_prev_change_y;
 
 		// If x and y velocity is not 0
-		bool moving;
+		bool m_moving;
 
-		bool collided;
+		bool m_collided;
 
 		// True when hit by any enemy
-		bool isHit;
+		bool m_isHit;
 
 		// If True remove TextBlock from game
-		bool remove;
+		bool m_remove;
 
 		void update(float deltaTime);
 
@@ -69,6 +65,12 @@ namespace GameEngine
 		void stop();
 		void collision(Sprite& sprite);
 		std::string to_string() const;
+
+	private:
+		float m_scaleFactor;
+		int m_adjustedTextblockWidth;
+		void LoadColorVector();
+		void ScaleTextBlock(int textSize, int blockWidth);
 	};
 }
 
